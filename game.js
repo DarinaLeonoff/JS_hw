19765a8399f458d7580da564677275987b99110d
Darina <dashunkaleo@yandex.ru>
2020-10-03 10:52:40 +0300

    figure
    add fiure

--- game.js
+++ game.js
@@ -2,7 +2,8 @@ var elem = document.querySelector('#block');
 var table = document.createElement('table');
 var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 var numb = ['1', '2', '3', '4', '5', '6', '7', '8'];
-var figure = ['&#9814;', '&#9816;', '&#9815;', '&#9813;', '&#9812;', '&#9815;', '&#9816;', '&#9814;', '&#9817;'];
+var figureW = ['-106px -45px', '-214px -45px', '-160px -45px', '-50px -45px', '0 -45px', '-160px -45px', '-214px -45px', '-106px -45px', '-269px -45px'];
+var figureB = ['-106px 0', '-214px 0', '-160px 0', '-50px 0', '0 0', '-160px 0', '-214px 0', '-106px 0', '-269px 0'];
 var names = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook', 'pown'];
 
 elem.insertAdjacentHTML("afterbegin", '<div class="container">');
@@ -32,7 +33,7 @@ for (var j = 0; j < cell.length; j++) {
     if (((i % 2 !== 0) && (j % 2 !== 0)) || ((i % 2 == 0) && (j % 2 == 0))) {
         cell[j].style.backgroundColor = "white";
     } else {
-        cell[j].style.backgroundColor = "black";
+        cell[j].style.backgroundColor = "#2F4F4F";
     };
 
     if (j == 7 || j == 7 * (i + 1) + i) {
@@ -71,8 +72,26 @@ listMaker(numb, 'col', '.right');
 
 
 function figureAccom(fig, name, str) {
+    if (str == 0 || str == 7) {
+        var j = 0;
+        for (var i = 8 * str; i < 8 * (str + 1); i++) {
+            cell[i].classList.add(name[j]);
+            cell[i].style.backgroundImage = 'url(kisspng-chess-piece-white-and-black-in-chess-queen-clip-ar-chess-transparent-png-5a76fcfe659344.1089993215177474544161.png)';
+            cell[i].style.backgroundPosition = fig[j];
+            j++;
 
+        }
+    } else {
+        for (var i = 8 * str; i < 8 * (str + 1); i++) {
+            cell[i].classList.add(name[8]);
+            cell[i].style.backgroundImage = 'url(kisspng-chess-piece-white-and-black-in-chess-queen-clip-ar-chess-transparent-png-5a76fcfe659344.1089993215177474544161.png)';
+            cell[i].style.backgroundPosition = fig[8];
+        }
+        return cell;
+    }
 }
 
-var firstLine = figureAccom(figure, names, 0);
-
+var firstW = figureAccom(figureW, names, 0);
+var firstB = figureAccom(figureB, names, 7);
+var pownW = figureAccom(figureW, names, 1);
+var pownB = figureAccom(figureB, names, 6);
